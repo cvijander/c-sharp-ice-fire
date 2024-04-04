c8de1c595b96422b438cf549c46422ef1d27cb4a
cvijander <cvija85@gmail.com>
2024-04-04 19:26:43 +0200

    3 lekcija Math clasa

---
+++ 3lekcija funkcije i petlje/Math clasa/Math clasa/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// proracun kvadratnog korena 
+double x = 50.0;
+double y = Math.Sqrt(x);
+
+// dizanje na 5 stepen
+double s = 50.0;
+double z = Math.Pow(s, 5);
+
+
+double b = 2.0;
+double a = 1;
+double c = 4;
+
+double m = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+
+
+Random random = new Random();
+int broj1 =random.Next(0,10);  // vraca slucaj ceo broj izmedju 0 i 9
+int broj2 = random.Next(50, 100);  //vraca slucajna ceo broj izmedju 50 i 99
+
+int broj3 = random.Next(20, 60);  // vraca slucajan broj izmedju 20 i 59
+
+
